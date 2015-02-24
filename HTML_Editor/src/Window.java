be1eb20f7863baf0a8a5029c6b5c12b42b6b4cf6
Jesse <roux.jesse@gmail.com>
2015-02-24 12:33:06 -0500

    Modified gitignore and added window

--- HTML_Editor/src/Window.java
+++ HTML_Editor/src/Window.java
@@ -1,4 +1,19 @@
+import javax.swing.*;
 
-public class Window {
-
+public class Window extends javax.swing.JFrame {
+	public Window() {
+		init();
+	}
+	
+	private void init() {
+		setTitle("HTML Editor");
+		setSize(640, 480);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		Window w = new Window();
+		w.setVisible(true);
+	}
 }
