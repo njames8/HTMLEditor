b04e353ff0b2d3cc7ddbf8602d73963ae150a0d3
Nick James <nxj2348@rit.edu>
2015-02-26 10:05:39 -0500

    Added file explorer

--- HTML_Editor/src/Window.java
+++ HTML_Editor/src/Window.java
@@ -20,12 +20,22 @@ public class Window extends javax.swing.JFrame {
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
+		JMenuItem mntmOpen = new JMenuItem("Open File");
+		mntmOpen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser f = new JFileChooser();
+				f.showOpenDialog(Window.this);
+			}
+		});
+		
 		JMenuItem mntmClose = new JMenuItem("Close");
 		mntmClose.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				System.exit(0);
 			}
 		});
+		
+		mnFile.add(mntmOpen);
 		mnFile.add(mntmClose);
 		
 		JMenu mnEdit = new JMenu("Edit");
