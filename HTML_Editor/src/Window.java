6df1f20b00135f15ce7dd34f08c14f2e34106e92
Jesse <roux.jesse@gmail.com>
2015-02-26 12:19:30 -0500

    Added a minimum window size

--- HTML_Editor/src/Window.java
+++ HTML_Editor/src/Window.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
+
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -10,19 +12,25 @@ public class Window extends javax.swing.JFrame {
 	
 	private void init() {
 		setTitle("HTML Editor");
+		setMinimumSize(new Dimension(320, 240));
 		setSize(640, 480);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		// Create menu bar
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
+		/*
+		 * File
+		 */
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
 		JMenuItem mntmOpen = new JMenuItem("Open File");
 		mntmOpen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				// TODO: make this a command
 				JFileChooser f = new JFileChooser();
 				f.showOpenDialog(Window.this);
 			}
@@ -38,9 +46,15 @@ public class Window extends javax.swing.JFrame {
 		mnFile.add(mntmOpen);
 		mnFile.add(mntmClose);
 		
+		/*
+		 * Edit
+		 */
 		JMenu mnEdit = new JMenu("Edit");
 		menuBar.add(mnEdit);
 		
+		/*
+		 * View
+		 */
 		JMenu mnView = new JMenu("View");
 		menuBar.add(mnView);	
 		
