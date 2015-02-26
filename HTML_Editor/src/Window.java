555c66fe72f53a89a5ec22d179b387fec42aab13
Nick James <nxj2348@rit.edu>
2015-02-26 09:54:08 -0500

    Added menu bar and close action

--- HTML_Editor/src/Window.java
+++ HTML_Editor/src/Window.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Window extends javax.swing.JFrame {
 	public Window() {
@@ -10,6 +13,27 @@ public class Window extends javax.swing.JFrame {
 		setSize(640, 480);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmClose = new JMenuItem("Close");
+		mntmClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		mnFile.add(mntmClose);
+		
+		JMenu mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
+		
+		JMenu mnView = new JMenu("View");
+		menuBar.add(mnView);	
+		
 	}
 	
 	public static void main(String[] args) {
