5a9fbb1b3526555a21f16ec553ba1faa35fda822
Nick James <nxj2348@rit.edu>
2015-02-26 13:33:20 -0500

    added close file

---
+++ HTML_Editor/src/commands/Close.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package commands;
+
+import ui.EditorWindow;
+
+/**
+ * @author Nick James
+ *
+ */
+public class Close implements Command {
+
+	
+	public Close(){
+		
+	}
+	
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		System.exit(0);
+	}
+
+}
