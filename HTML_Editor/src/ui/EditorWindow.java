2a458679ea5e62f17740076c50561eae92f57645
Matthew <mrg18bd@gmail.com>
2015-03-01 14:52:58 -0500

    commented

--- HTML_Editor/src/ui/EditorWindow.java
+++ HTML_Editor/src/ui/EditorWindow.java
@@ -9,11 +9,6 @@ import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.InputMethodListener;
-import java.awt.event.InputMethodEvent;
-
 
 public class EditorWindow extends javax.swing.JFrame {
 	private JTabbedPane tabbedPane;
@@ -31,101 +26,86 @@ public class EditorWindow extends javax.swing.JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		// Create menu bar
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
 		tabbedPane = new JTabbedPane();
 		getContentPane().add(tabbedPane, BorderLayout.CENTER);
 		
 		NewTab(tabbedPane);
 		
+		// Create menu bar
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+				
 		/*
 		 * File
 		 */
 		JMenu mnFile = new JMenu("File");
-		menuBar.add(mnFile);
-		
-		JMenuItem mntmNew = new JMenuItem("New");
+
+		JMenuItem mntmNew = new JMenuItem("New");//Makes the menu button for making a new tab
 		mntmNew.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				/*NewFile n = new NewFile(new JFileChooser(), EditorWindow.this);
-				n.execute();*/
-
-				NewTab(tabbedPane);
-
+				NewTab(tabbedPane);//Adds new tab to the window
 			}
 		});
 		
-		JMenuItem mntmOpen = new JMenuItem("Open File");
+		JMenuItem mntmOpen = new JMenuItem("Open File");//Makes the menu button for opening files
 		mntmOpen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-
+				//calls the command to open file and adds it to the window.
 				OpenFile o = new OpenFile(new JFileChooser(), EditorWindow.this, tabbedPane);
-
 				o.execute();
 			}
 		});
 		
-		JMenuItem mntmClose = new JMenuItem("Close");
+		JMenuItem mntmClose = new JMenuItem("Close");//Makes the close button for closing the window. 
 		mntmClose.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				Close c = new Close();
+				Close c = new Close();//Makes the command to close
 				c.execute();
 			}
 		});
 		
-		JMenuItem mntmNewFile = new JMenuItem("New File");
-		mntmNewFile.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		
-		mnFile.add(mntmNew);
-		mnFile.add(mntmOpen);
-		
-		JMenuItem mntmSave = new JMenuItem("Save");
+		JMenuItem mntmSave = new JMenuItem("Save");//Makes the Save button for saving files
 		mntmSave.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent arg0){
+				//finds the current tab
+				//Makes the command to save the file in the current tab.
 				SaveFile f = new SaveFile((Tab)(tabbedPane.getComponentAt(tabbedPane.getSelectedIndex())), EditorWindow.this);
 				f.execute();
 			}
 		});
 		
-		mnFile.add(mntmSave);
-		
-		JMenuItem mntmCloseTab = new JMenuItem("Close Tab");
+		JMenuItem mntmCloseTab = new JMenuItem("Close Tab");//Makes the button for closing the current tab.
 		mntmCloseTab.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-
-				System.out.println(tabbedPane.getSelectedIndex());
-				CloseTab t = new CloseTab(tabbedPane.getComponentAt(tabbedPane.getSelectedIndex()),tabbedPane);// TODO Pass current tab
+				//Makes the command to close the current tab.
+				CloseTab t = new CloseTab(tabbedPane.getComponentAt(tabbedPane.getSelectedIndex()),tabbedPane);
 				t.execute();
-
 			}
 		});
 		
-		
-		
-		JMenuItem mntmSaveAs = new JMenuItem("Save As...");
-		mnFile.add(mntmSaveAs);
-		mnFile.add(mntmCloseTab);
-		mnFile.add(mntmClose);
+		JMenuItem mntmSaveAs = new JMenuItem("Save As...");//Makes the Save button for saving files
 		
 		/*
 		 * Edit
 		 */
-		JMenu mnEdit = new JMenu("Edit");
-		menuBar.add(mnEdit);
-		
+		JMenu mnEdit = new JMenu("Edit");//makes the edit menu 
 		/*
 		 * View
 		 */
-		JMenu mnView = new JMenu("View");
+		JMenu mnView = new JMenu("View");//makes the view menu
+		
+		//adds all the menu buttons and menu headers to the window.
+		menuBar.add(mnFile);
+			mnFile.add(mntmNew);
+			mnFile.add(mntmOpen);
+			mnFile.add(mntmSave);
+			mnFile.add(mntmSaveAs);
+			mnFile.add(mntmCloseTab);
+			mnFile.add(mntmClose);
+		menuBar.add(mnEdit);
 		menuBar.add(mnView);
+		
 	}
 
 
