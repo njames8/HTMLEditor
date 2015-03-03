02cafacfd8ab354a4cf029dcda4ccedbb95a55a3
Adam Walsh <adamwalsh14@gmail.com>
2015-03-03 13:04:05 -0500

    Moved addition of tag to stack

--- HTML_Editor/src/parsing/Parser.java
+++ HTML_Editor/src/parsing/Parser.java
@@ -8,13 +8,15 @@ public class Parser {
 		String[] parts = html.split("<");
 		for (String part : parts) {
 			String tag = part.split(">")[0];
-			tagStack.add(tag);
 			if (tag.startsWith("/")) {
 				String expectedTag = tagStack.pop();
 				if (!expectedTag.equals(tag)) {
 					throw new SyntaxException(1, "Expecting a closing tag of " +
 				expectedTag);
 				}
+			}
+			else {
+				tagStack.add(tag);
 			}
 			System.out.println(tag);
 		}
