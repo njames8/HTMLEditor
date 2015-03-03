170efd6e067dd0eea488468ecc285433b72b2278
John Mullen <john@aspect.gg>
2015-03-03 14:14:36 -0500

    Added Indenter

---
+++ HTML_Editor/src/Formatting/Indenter.java
@@ -0,0 +1,30 @@
+package Formatting;
+import java.util.*;
+
+
+public class Indenter {
+	public static String openIndent(String html){
+		int depth = 0; //used to determine number of spaces
+		String returnText = ""; //text to be returned
+		String[] parts = html.split("(?=<)"); //splits current text into sections per tag
+		for (String part : parts){
+			//finds appropriate depth and lines up number of spacing iterations to add.
+			int count = 0;
+			if(part.charAt(1) == '/'){
+				depth -= 1;
+				count = depth;
+			}
+			else{
+				count = depth;
+				depth += 1;
+			}
+			while(count > 0){
+				part = "    " + part;
+				count -= 1;
+			}
+			returnText = returnText + part;
+		}
+		return returnText;	
+	}
+
+}
