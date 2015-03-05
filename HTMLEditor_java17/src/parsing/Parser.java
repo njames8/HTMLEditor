754a93c3e65c24e9da71cebe45912c563534917e
Adam Walsh <adamwalsh14@gmail.com>
2015-03-05 13:28:52 -0500

    Deleted old proj with jre 1.8, updated parser to support self closing link / script tags

--- HTMLEditor_java17/src/parsing/Parser.java
+++ HTMLEditor_java17/src/parsing/Parser.java
@@ -2,6 +2,18 @@ package parsing;
 import java.util.*;
 
 public class Parser {
+	//*
+	public static void main(String[] args) {
+		try {
+			Parse("<html>\n<body>\n<p>\n<script src=\"example\"/>\n</p>\n</body>\n</html>");
+		} catch (SyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}//*/
+	private static String[] allowedTags = {"html", "body", "header", "footer",
+		"b", "p", "i", "li", "ol", "ul", "script", "a", "h1", "h2", "h3", "h4",
+		"h5", "h6", "link"};//FIXME add rest of supported tags
 	public static List<SyntaxError> Parse(String html) throws SyntaxException {
 		Stack<String> tagStack = new Stack<String>();
 		ArrayList<SyntaxError> errs = new ArrayList<SyntaxError>();
@@ -17,10 +29,31 @@ public class Parser {
 				}
 			}
 			else {
+				if (!isTagSupported(tag.split(" ")[0])) {
+					continue;
+				}//skip unsupported tag
+				else if (tag.startsWith("script")) {//handle script tag
+					if (tag.endsWith("/")) {//self-closed tag
+						continue;//ignore
+					}
+				}
+				else if (tag.startsWith("link")) {//handle link tag
+					if (tag.endsWith("/")) {//self-closed tag
+						continue;//ignore
+					}
+				}
 				tagStack.add(tag);
 			}
 			System.out.println(tag);
 		}
 		return errs;
+	}
+	
+	private static boolean isTagSupported(String tag) {
+		for (String t : allowedTags) {
+			if (t.equalsIgnoreCase(tag))
+				return true;
+		}
+		return false;
 	}
 }
--- HTML_Editor/src/parsing/SyntaxException.java
+++
@@ -1,8 +0,0 @@
-package parsing;
-
-public class SyntaxException extends Exception {
-	public SyntaxException(int lineNum, String msg) {
-		super("Syntax exception @ " + lineNum 
-				+ " " + msg);
-	}
-}
