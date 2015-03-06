fd8fcf3d3476714bfa9b1ac40f5a468331f426f0
Adam Walsh <adamwalsh14@gmail.com>
2015-03-05 20:38:23 -0500

    More logic improvements to parser

--- HTMLEditor_java17/src/parsing/Parser.java
+++ HTMLEditor_java17/src/parsing/Parser.java
@@ -2,9 +2,10 @@ package parsing;
 import java.util.*;
 
 public class Parser {
-	private static String[] allowedTags = {"html", "body", "header", "footer",
+	private static String[] allowedTags = {"html", "body", "head", "footer",
 		"b", "p", "i", "li", "ol", "ul", "script", "a", "h1", "h2", "h3", "h4",
-		"h5", "h6", "link"};//FIXME add rest of supported tags
+		"h5", "h6", "link", "meta", "title", "link"};//FIXME add rest of supported tags
+	private static String[] optionalCloseTags = { "link" };
 	public static void Parse(String html) throws SyntaxException {
 		Stack<String> tagStack = new Stack<String>();
 		String[] parts = html.split("<");
@@ -15,11 +16,15 @@ public class Parser {
 			if (tagCount == 2)
 				throw new SyntaxException(1, "Parsing failed, only one tag found.");
 		for (String part : parts) {
+			if (part.startsWith("!"))//skip comments
+				continue;
 			String tag = part.split(">")[0];
 			if (tag.startsWith("/")) {//ending tag
 				String expectedTag = tagStack.pop();
 				tag = tag.substring(1);//cut off end tag slash
-				if (!expectedTag.equals(tag)) {//matches last opened tag
+				if (!expectedTag.equals(tag)) {//doesn't match last opened tag
+					if (isTagOptionalClose(expectedTag))//it's okay, it's optional
+						continue;
 					throw new SyntaxException(1, "Parsing failed. Expecting a closing tag of '" +
 				expectedTag + "'");
 				}
@@ -33,7 +38,6 @@ public class Parser {
 				}
 				tagStack.add(tagName);
 			}
-			System.out.println(tag);
 		}
 	}
 	
@@ -42,6 +46,13 @@ public class Parser {
 			if (t.equalsIgnoreCase(tag))
 				return true;
 		}
+		return false;
+	}
+	
+	private static boolean isTagOptionalClose(String tag) {
+		for (String t : optionalCloseTags)
+			if (t.equalsIgnoreCase(tag))
+				return true;
 		return false;
 	}
 }
