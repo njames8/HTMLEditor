bd47b8a3d299a364e7eab3212ae91d656fb00ab0
Adam Walsh <adamwalsh14@gmail.com>
2015-03-05 20:28:42 -0500

    Logic updates to parser

--- HTMLEditor_java17/src/parsing/Parser.java
+++ HTMLEditor_java17/src/parsing/Parser.java
@@ -5,29 +5,36 @@ public class Parser {
 	private static String[] allowedTags = {"html", "body", "header", "footer",
 		"b", "p", "i", "li", "ol", "ul", "script", "a", "h1", "h2", "h3", "h4",
 		"h5", "h6", "link"};//FIXME add rest of supported tags
-	public static List<SyntaxError> Parse(String html) throws SyntaxException {
+	public static void Parse(String html) throws SyntaxException {
 		Stack<String> tagStack = new Stack<String>();
-		ArrayList<SyntaxError> errs = new ArrayList<SyntaxError>();
 		String[] parts = html.split("<");
+		int tagCount = parts.length;
+		if (tagCount == 1)
+			throw new SyntaxException(1, "Parsing failed, no tags found.");
+		else 
+			if (tagCount == 2)
+				throw new SyntaxException(1, "Parsing failed, only one tag found.");
 		for (String part : parts) {
 			String tag = part.split(">")[0];
-			if (tag.startsWith("/")) {
+			if (tag.startsWith("/")) {//ending tag
 				String expectedTag = tagStack.pop();
 				tag = tag.substring(1);//cut off end tag slash
-				if (!expectedTag.equals(tag)) {
+				if (!expectedTag.equals(tag)) {//matches last opened tag
 					throw new SyntaxException(1, "Parsing failed. Expecting a closing tag of '" +
 				expectedTag + "'");
 				}
 			}
-			else {
-				if (!isTagSupported(tag.split(" ")[0]) || tag.endsWith("/")) {
-					continue;
-				}//skip unsupported tag
-				tagStack.add(tag);
+			else {//non-ending tag
+				if (tag.equals("")) { continue; }
+				String tagName = tag.split(" ")[0];
+				if (!isTagSupported(tagName) || tagName.endsWith("/")) {
+					throw new SyntaxException(1, "Parsing failed. Invalid tag found, '" +
+				tagName + "'");
+				}
+				tagStack.add(tagName);
 			}
 			System.out.println(tag);
 		}
-		return errs;
 	}
 	
 	private static boolean isTagSupported(String tag) {
