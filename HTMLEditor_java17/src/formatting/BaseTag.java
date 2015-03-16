eb199bb017cabf735ecf6db78554f87f1e5c773e
Matthew <mrg18bd@gmail.com>
2015-03-15 21:55:23 -0400

    added treverse to the BaseTag

--- HTMLEditor_java17/src/formatting/BaseTag.java
+++ HTMLEditor_java17/src/formatting/BaseTag.java
@@ -12,6 +12,7 @@ public class BaseTag {
 	private String tag;//tag value e.g. 'p' for a <p> tag
 	private Boolean collapsed;
 	private ArrayList<BaseTag> children;
+	private int lineNumber;
 	public BaseTag() {
 		this.children = new ArrayList<BaseTag>();
 	}
@@ -22,5 +23,36 @@ public class BaseTag {
 	
 	public String GetContent() {
 		return "";//TODO
+	}
+	
+	/**
+	 * @author Matthew Gallagher
+	 *  
+	 * @param counter
+	 * @return
+	 */
+	public int traverse (int counter){
+		this.setLineNumber(counter);
+		if(this.children.size() > 0){
+			counter = counter + 1;
+			for(int i = 0; i < children.size(); i++){
+				counter = children.get(i).traverse(counter);
+			}
+		}
+		return counter + 1;
+	}
+
+	/**
+	 * @return the lineNumber
+	 */
+	public int getLineNumber() {
+		return lineNumber;
+	}
+
+	/**
+	 * @param lineNumber the lineNumber to set
+	 */
+	public void setLineNumber(int lineNumber) {
+		this.lineNumber = lineNumber;
 	}
 }
